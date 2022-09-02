1aae33440885ed01f2aa056b6debd2fe1eaa7122
Ronaldkubasu <ronald.kubasu@student.moringaschool.com>
2022-09-02 06:27:42 +0300

    commented on the index.html lines

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 
+// shoe purchase starts here
 let erroParagraph = document.getElementById("error")
 console.log(erroParagraph)
 
@@ -7,6 +8,10 @@ function purchase () {
     erroParagraph.textContent  = "Congrats!!! You've successfully puchased your j4 pro nike shoe"
 }
 
+// shoe purchase ends here
+
+
+// Calculator starts here
 let num1 = 8
 let num2 = 4
 document.getElementById("num1_el").textContent = num1
@@ -35,7 +40,38 @@ function Multiply() {
     let result = num1 * num2
     sumEl.textContent = "sum: " + result
 }
+// Calculator ends here
 
-let firstCard = 6
-let secondCard = 9
-let sum = firstCard = secondCard
+
+
+// Our casino game starts here
+let firstCard = 10
+let secondCard = 11
+let product = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+
+let  messageEl = document.getElementById("message_el")
+console.log(message_el)
+
+let productEl = document.getElementById("sum_el")
+
+function startGame () {
+
+    productEl.textContent = "sum " + product
+if (product <= 20) {
+    message = "Do you want to draw a new card?" 
+}else if (product === 21) {
+    message = "Wohoo! You've got Blackjack"
+    hasBlackJack = true
+}else {
+    message = "You are out of the game"
+    isAlive = false
+}
+
+message_el.textContent = message
+
+}
+
+// Our casino ends here
